dedc7d406df23620141c1303b5325eb97e03a597
Sergio Andrés SÁNCHEZ <103612998+SergioAndresSanchezTdfEdu@users.noreply.github.com>
2023-10-15 09:22:12 -0300

    ciudad completo y sin cardinalidad

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Ciudad } from './entities/ciudad.entity';
 import { CiudadService } from './ciudad.service';
+import { CiudadDTO } from './dto/ciudad.dto';
 
 @Controller('ciudad')
 export class CiudadController {
@@ -8,13 +9,34 @@ export class CiudadController {
     constructor(private readonly ciudadService: CiudadService) {}
 
     @Get('raw')
-    async getAllRaw(): Promise<Ciudad[]> {
+    async getAllRaw(): Promise<CiudadDTO[]> {
         return await this.ciudadService.findAllRaw();
     }
 
     @Get('orm')
-    async getAllOrm(): Promise<Ciudad[]> {
+    async getAllOrm(): Promise<CiudadDTO[]> {
         return await this.ciudadService.findAllOrm();
     }
+
+    @Get(':id')
+    async getId(@Param('id')id:number): Promise<CiudadDTO> {
+        return await this.ciudadService.findById(id);
+    }
+
+    @Post('crear')
+    async crearCiudad(@Body() ciudadDTO: CiudadDTO): Promise<boolean> {
+        return await this.ciudadService.crearCiudad(ciudadDTO);
+    }
+    
+    @Put('actualizar/:id')
+    async actualizarCiudadId(@Body() ciudadDTO: CiudadDTO, @Param('id')id:number): Promise<String> {
+        return await this.ciudadService.actualizarCiudadId(ciudadDTO,id);
+    }
+
+    @Delete('eliminar/:id')
+        async eliminarCiudadId(@Param('id')id:number): Promise<Ciudad> {
+            return await this.ciudadService.eliminarCiudadId(id);
+    }
+
 
 }
--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Ciudad } from './entities/ciudad.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+import { CiudadDTO } from './dto/ciudad.dto';
 
 @Injectable()
 export class CiudadService {
@@ -13,7 +14,7 @@ export class CiudadService {
     private readonly ciudadRepository: Repository<Ciudad>
     ){}
 
-    async findAllRaw() : Promise<Ciudad[]> {
+    async findAllRaw() : Promise<CiudadDTO[]> {
         this.ciudades = [];
         let datos = await this.ciudadRepository.query("SELECT * FROM ciudad");
 
@@ -26,9 +27,87 @@ export class CiudadService {
 
     }
 
-    async findAllOrm(): Promise<Ciudad[]> {
+    async findAllOrm(): Promise<CiudadDTO[]> {
         return await this.ciudadRepository.find();    
     }
 
+    async findById(id:number): Promise<CiudadDTO> {
+        try {
+            const criterio : FindOneOptions = { where: {id:id} };
+            let ciudad : Ciudad = await this.ciudadRepository.findOne(criterio);
+            if (ciudad)
+                return ciudad;
+            else
+                throw new Error('No se encuentra la ciudad');
+        }
+        catch(error){
+            throw new HttpException({
+                status: HttpStatus.CONFLICT,
+                error: 'Error en Ciudad: ' + error
+            },HttpStatus.NOT_FOUND)
+        }
+        //return await this.ciudadRepository.findOne(id);
+    }
 
+    async crearCiudad(ciudadDTO : CiudadDTO): Promise<boolean>{
+        //let ciudad : Ciudad = new Ciudad(ciudadDTO.nombre);
+        //this.ciudadRepository.save(ciudad);
+        try{
+            let ciudad : Ciudad = await this.ciudadRepository.save(new Ciudad(ciudadDTO.nombre));
+            if(ciudad)
+                return true;
+            else
+            throw new Error('No se puede crear la ciudad');
+        }
+        catch(error){
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'Error en Ciudad: ' + error
+            },HttpStatus.NOT_FOUND)
+        }
+    }
+
+    async actualizarCiudadId(ciudadDTO : CiudadDTO, id : number): Promise<String>{
+        try{
+            const criterio : FindOneOptions = {where: {id:id}}
+            let ciudad : Ciudad = await this.ciudadRepository.findOne(criterio);
+            
+            if(!ciudad)
+                throw new Error('No se pudo encontrar la ciudad a modificar');
+            else{
+                let ciudadVieja = ciudad.getNombre();
+                ciudad.setNombre(ciudadDTO.nombre);
+                ciudad = await this.ciudadRepository.save(ciudad);
+
+                return `OK - Se cambió: ${ciudadVieja} por ${ciudadDTO.nombre}`;
+            }
+        }
+        catch(error){
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'Error en Ciudad: ' + error
+            },HttpStatus.NOT_FOUND)
+        }
+    }
+
+    async eliminarCiudadId(id:number) : Promise<any>{
+        try{
+            const criterio : FindOneOptions = {where: {id:id}}
+            let ciudad : Ciudad = await this.ciudadRepository.findOne(criterio);
+            
+            if(!ciudad)
+                throw new Error('No se pudo eliminar la ciudad');
+            else{
+                await this.ciudadRepository.remove(ciudad);
+                return { id:id,
+                        message:'Se elimino exitosamente la ciudad'};
+            }
+        }
+        catch(error){
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'Error en Ciudad: ' + error
+            },HttpStatus.NOT_FOUND)
+        }
+    }
 }
